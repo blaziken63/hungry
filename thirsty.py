add10b25cb12f77240ac4f07fa5ae17f6946278a
Rohit Kohli <158089898+blaziken63@users.noreply.github.com>
2025-04-23 09:47:14 +0530

    Update thirsty.py

--- thirsty.py
+++ thirsty.py
@@ -1,6 +1,4 @@
 thirst=input("are you thirsty?")
-# Error line--> if thirst=='yes' and thirst=='y' and thirst=='Y':
-# Resolved
 if thirst=='yes' or thirst=='y' or thirst=='Y':
   print("drink water")
 else:
